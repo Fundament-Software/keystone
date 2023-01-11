846cc6da56b1fa37bccd2d364820f02cfb5344a0
ugandalf <driwer99@interia.pl>
2023-01-11 03:01:22 +0100

    A bunch of remaining methods

--- core/src/http/path.rs
+++ core/src/http/path.rs
@@ -11,52 +11,11 @@ use hyper::{
 };
 use hyper_tls::HttpsConnector;
 
-fn get_uri(authority: &Authority) -> hyper::Uri {
-    let mut parts = Parts::default();
-    parts.scheme = Some("https".parse().unwrap());
-    parts.authority = Some(authority.clone()); // TODO Clone?
-    parts.path_and_query = Some("".parse().unwrap());
-    // TODO parts.path_and_query
-    hyper::Uri::from_parts(parts).unwrap() // TODO unwrap
-}
-
-pub struct PathImpl {
-    // TODO Client is cheap to clone and cloning is the recommended way to share a Client. The underlying connection pool will be reused.
-    // We're not doing that - should we? And if so, does it trickle down?
-    https_client: hyper::Client<HttpsConnector<HttpConnector>>,
-    query: Vec<(String, String)>,
-    headers: HeaderMap,
-    query_modifiable: bool,
-    path_modifiable: bool,
-    headers_modifiable: bool,
-    authority: Authority,
-}
-
-impl PathImpl {
-    pub fn new(path_name: String) -> Self {
-        // TODO String should be something that coerces to String
-        let connector = HttpsConnector::new();
-        let https_client = hyper::Client::builder().build::<_, hyper::Body>(connector);
-        PathImpl {
-            query: vec![],
-            headers: HeaderMap::new(),
-            path_modifiable: true,
-            query_modifiable: true,
-            headers_modifiable: true,
-            https_client,
-            authority: Authority::from_str(path_name.as_str()).unwrap(), // TODO unwrap
-        }
-    }
-
-    fn http_request_impl(
-        &mut self,
-        mut results: capnp::capability::Results<crate::http_capnp::path::http_result::Owned>,
-        verb: Path::HttpVerb,
-    ) -> Promise<(), capnp::Error> {
-        let future = self.http_request(Path::HttpVerb::Get, None);
+macro_rules! http_request_promise {
+    ($results:expr, $future:expr) => {
         Promise::<_, capnp::Error>::from_future(async move {
-            let mut results_builder = results.get();
-            let response = future
+            let mut results_builder = $results.get().init_result();
+            let response = $future
                 .await
                 .map_err(|err| capnp::Error::failed(err.to_string()))?;
             results_builder.set_status_code(response.status().as_u16());
@@ -77,16 +36,54 @@ impl PathImpl {
             results_builder.reborrow().set_body(body.as_str());
             Ok(())
         })
+
+    };
+}
+
+#[derive(Clone)]
+pub struct PathImpl {
+    // TODO Client is cheap to clone and cloning is the recommended way to share a Client. The underlying connection pool will be reused.
+    // We're not doing that - should we? And if so, does it trickle down?
+    https_client: hyper::Client<HttpsConnector<HttpConnector>>,
+    query: Vec<(String, String)>,
+    path_list: Vec<String>,
+    headers: HeaderMap,
+    query_modifiable: bool,
+    path_modifiable: bool,
+    headers_modifiable: bool,
+    authority: Authority,
+    verb_whitelist: Vec<Path::HttpVerb>, //TODO consider a set instead
+}
+
+impl PathImpl {
+    pub fn new(path_name: String) -> Self {
+        // TODO String should be something that coerces to String
+        let connector = HttpsConnector::new();
+        let https_client = hyper::Client::builder().build::<_, hyper::Body>(connector);
+        PathImpl {
+            https_client,
+            query: vec![],
+            path_list: vec![],
+            headers: HeaderMap::new(),
+            path_modifiable: true,
+            query_modifiable: true,
+            headers_modifiable: true,
+            verb_whitelist: vec![],
+            authority: Authority::from_str(path_name.as_str()).unwrap(), // TODO unwrap
+        }
+    }
+
+    fn get_uri(&self) -> hyper::Uri {
+        let mut parts = Parts::default();
+        parts.scheme = Some("https".parse().unwrap());
+        parts.authority = Some(self.authority.clone()); // TODO Clone?
+        parts.path_and_query = Some("".parse().unwrap());
+        // TODO parts.path_and_query
+        hyper::Uri::from_parts(parts).unwrap() // TODO unwrap
     }
 
     // Such function isn't part of specification, but I think it'd simplify interface
     fn http_request(&self, verb: Path::HttpVerb, body: Option<String>) -> ResponseFuture {
-        let has_empty_body = [
-            Path::HttpVerb::Get,
-            Path::HttpVerb::Head,
-            Path::HttpVerb::Options,
-        ]
-        .contains(&verb);
         let method = match verb {
             Path::HttpVerb::Get => "GET",
             Path::HttpVerb::Head => "HEAD",
@@ -96,9 +93,20 @@ impl PathImpl {
             Path::HttpVerb::Options => "OPTIONS",
             Path::HttpVerb::Patch => "PATCH",
         };
-        let mut request_builder = hyper::Request::builder()
-            .method(method)
-            .uri(get_uri(&self.authority));
+        // TODO Change type signature and contents of all methods *sigh*
+        // if !self.verb_whitelist.is_empty() && self.verb_whitelist.contains(&verb) {
+        //     return Promise::err(capnp::Error::failed(format!(
+        //         "{method} is not on a whitelist and can't be executed."
+        //     )));
+        // }
+        let has_empty_body = [
+            Path::HttpVerb::Get,
+            Path::HttpVerb::Head,
+            Path::HttpVerb::Options,
+        ]
+        .contains(&verb);
+
+        let mut request_builder = hyper::Request::builder().method(method).uri(self.get_uri());
         for (key, value) in self.headers.iter() {
             request_builder = request_builder.header(key, value);
         }
@@ -118,8 +126,20 @@ impl Path::Server for PathImpl {
         params: Path::QueryParams,
         mut results: Path::QueryResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
+        if !self.query_modifiable {
+            return Promise::err(capnp::Error::failed(format!(
+                "can't add to query, because it was finalized"
+            )));
+        }
+        let values = pry!(pry!(params.get()).get_values());
+        let mut return_path = self.clone();
+        for value in values.iter() {
+            let k = pry!(value.get_key());
+            let v = pry!(value.get_value());
+            return_path.query.push((k.to_string(), v.to_string()));
+        }
+        let client = capnp_rpc::new_client(return_path);
+        results.get().set_result(client);
         Promise::ok(())
     }
 
@@ -128,8 +148,19 @@ impl Path::Server for PathImpl {
         params: Path::PathParams,
         mut results: Path::PathResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
+        if !self.path_modifiable {
+            return Promise::err(capnp::Error::failed(format!(
+                "can't add to path, because it was finalized"
+            )));
+        }
+        let values = pry!(pry!(params.get()).get_values());
+        let mut return_path = self.clone();
+        for value in values.iter() {
+            let v = pry!(value);
+            return_path.path_list.push(v.to_string());
+        }
+        let client = capnp_rpc::new_client(return_path);
+        results.get().set_result(client);
         Promise::ok(())
     }
     // START OF IMPLEMENTATIONS THAT RETURN HTTP RESULT
@@ -139,30 +170,7 @@ impl Path::Server for PathImpl {
         mut results: Path::GetHttpResults,
     ) -> Promise<(), capnp::Error> {
         let future = self.http_request(Path::HttpVerb::Get, None);
-        //let future = self.https_client.get(get_uri(&self.authority));
-        Promise::<_, capnp::Error>::from_future(async move {
-            let mut results_builder = results.get().init_result();
-            let response = future
-                .await
-                .map_err(|err| capnp::Error::failed(err.to_string()))?;
-            results_builder.set_status_code(response.status().as_u16());
-            let len_response_headers: u32 = response.headers().len() as u32;
-            let header_iter = response.headers().iter().enumerate();
-            let mut results_headers = results_builder
-                .reborrow()
-                .init_headers(len_response_headers);
-            for (i, (key, value)) in header_iter {
-                let mut pair = results_headers.reborrow().get(i as u32);
-                pair.set_key(key.as_str());
-                pair.set_value(value.to_str().unwrap()); // TODO Another risky unwrap
-            }
-            let body_bytes = hyper::body::to_bytes(response.into_body())
-                .await
-                .map_err(|err| capnp::Error::failed(err.to_string()))?;
-            let body = String::from_utf8(body_bytes.to_vec()).unwrap(); // TODO unwrapping not what we want
-            results_builder.reborrow().set_body(body.as_str());
-            Ok(())
-        })
+        http_request_promise!(results, future)
     }
 
     fn head(
@@ -171,9 +179,7 @@ impl Path::Server for PathImpl {
         mut results: Path::HeadResults,
     ) -> Promise<(), capnp::Error> {
         let future = self.http_request(Path::HttpVerb::Head, None);
-
-        // TODO The same contents as in get Promise exactly
-        Promise::ok(())
+        http_request_promise!(results, future)
     }
 
     fn post(
@@ -182,9 +188,8 @@ impl Path::Server for PathImpl {
         mut results: Path::PostResults,
     ) -> Promise<(), capnp::Error> {
         let body = pry!(pry!(params.get()).get_body());
-        let future = self.http_request(Path::HttpVerb::Post, Some(body.to_string())); //TODO to_string should not be needed
-        self.http_request_impl(results.into(), Path::HttpVerb::Post)
-        // TODO The same contents as in get Promise exactly
+        let future = self.http_request(Path::HttpVerb::Post, Some(body.to_string())); //TODO to_string should not be needed in all those methods
+        http_request_promise!(results, future)
     }
 
     fn put(
@@ -192,9 +197,9 @@ impl Path::Server for PathImpl {
         params: Path::PutParams,
         mut results: Path::PutResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
-        Promise::ok(())
+        let body = pry!(pry!(params.get()).get_body());
+        let future = self.http_request(Path::HttpVerb::Put, Some(body.to_string()));
+        http_request_promise!(results, future)
     }
 
     fn delete(
@@ -202,9 +207,9 @@ impl Path::Server for PathImpl {
         params: Path::DeleteParams,
         mut results: Path::DeleteResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
-        Promise::ok(())
+        let body = pry!(pry!(params.get()).get_body());
+        let future = self.http_request(Path::HttpVerb::Delete, Some(body.to_string()));
+        http_request_promise!(results, future)
     }
 
     fn options(
@@ -212,9 +217,8 @@ impl Path::Server for PathImpl {
         _: Path::OptionsParams,
         mut results: Path::OptionsResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
-        Promise::ok(())
+        let future = self.http_request(Path::HttpVerb::Options, None);
+        http_request_promise!(results, future)
     }
 
     fn patch(
@@ -222,9 +226,9 @@ impl Path::Server for PathImpl {
         params: Path::PatchParams,
         mut results: Path::PatchResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
-        Promise::ok(())
+        let body = pry!(pry!(params.get()).get_body());
+        let future = self.http_request(Path::HttpVerb::Patch, Some(body.to_string()));
+        http_request_promise!(results, future)
     }
     // END OF IMPLEMENTATIONS THAT RETURN HTTP RESULT
     fn finalize_query(
@@ -232,8 +236,10 @@ impl Path::Server for PathImpl {
         _: Path::FinalizeQueryParams,
         mut results: Path::FinalizeQueryResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
+        let mut return_path = self.clone();
+        return_path.query_modifiable = false;
+        let client = capnp_rpc::new_client(return_path);
+        results.get().set_result(client);
         Promise::ok(())
     }
 
@@ -242,18 +248,30 @@ impl Path::Server for PathImpl {
         _: Path::FinalizePathParams,
         mut results: Path::FinalizePathResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
+        let mut return_path = self.clone();
+        return_path.path_modifiable = false;
+        let client = capnp_rpc::new_client(return_path);
+        results.get().set_result(client);
         Promise::ok(())
     }
 
     fn whitelist_verbs(
         &mut self,
-        _: Path::WhitelistVerbsParams,
+        params: Path::WhitelistVerbsParams,
         mut results: Path::WhitelistVerbsResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
+        // TODO No pry!
+        let verbs = pry!(pry!(params.get()).get_verbs());
+        let mut return_path = self.clone();
+        for verb in verbs.iter() {
+            let v = pry!(verb);
+            // Check uniqueness - would use HashSet, but enums aren't Hash by default
+            if !return_path.verb_whitelist.contains(&v) {
+                return_path.verb_whitelist.push(v);
+            }
+        }
+        let client = capnp_rpc::new_client(return_path);
+        results.get().set_result(client);
         Promise::ok(())
     }
 
@@ -262,8 +280,23 @@ impl Path::Server for PathImpl {
         params: Path::HeadersParams,
         mut results: Path::HeadersResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
+        if !self.headers_modifiable {
+            return Promise::err(capnp::Error::failed(format!(
+                "Can't add headers, because they were finalized"
+            )));
+        }
+        let headers = pry!(pry!(params.get()).get_headers());
+        let mut return_path = self.clone();
+        for header in headers.iter() {
+            let key = pry!(header.get_key());
+            let key = hyper::header::HeaderName::try_from(key).unwrap();
+            let value = pry!(header.get_value());
+            let value = hyper::http::HeaderValue::try_from(value).unwrap();
+            //TODO unwraps and pry!
+            return_path.headers.append(key, value);
+        }
+        let client = capnp_rpc::new_client(return_path);
+        results.get().set_result(client);
         Promise::ok(())
     }
 
@@ -272,25 +305,13 @@ impl Path::Server for PathImpl {
         _: Path::FinalizeHeadersParams,
         mut results: Path::FinalizeHeadersResults,
     ) -> Promise<(), capnp::Error> {
-        let value = todo!();
-        results.get().set_result(value);
+        let mut return_path = self.clone();
+        return_path.headers_modifiable = false;
+        let client = capnp_rpc::new_client(return_path);
+        results.get().set_result(client);
         Promise::ok(())
     }
 }
-
-// impl Path::Server for PathImpl {
-//     fn query(
-//         &mut self,
-//         params: Path::QueryParams,
-//         mut results: Path::QueryResults,
-//     ) -> Promise<(), capnp::Error> {
-//         let reader = pry!(params.get());
-//         let key = pry!(reader.get_key());
-//         let value = pry!(reader.get_value());
-//         self.query.push((String::from(key), String::from(value)));
-//         //results.get().set_path(self);
-//         Promise::ok(())
-//     }
 
 #[cfg(test)]
 mod tests {
@@ -300,7 +321,7 @@ mod tests {
     async fn get_test() {
         let path_client: Path::Client =
             capnp_rpc::new_client(PathImpl::new("www.example.org".to_string()));
-        let request = path_client.get_http_request();
+        let request = path_client.head_request();
         let result = request.send().promise.await.unwrap();
         let res = result.get().unwrap().get_result().unwrap();
         let body = res.get_body().unwrap();
